c2fea5db4ef636484bf05616a20a0b527ed5c499
shubhamdhorajkar <shubhamdhorajkar9975@gmail.com>
2025-12-24 13:14:52 +0530

    Configure Vite app for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import HomePage from "./Pages/HomePage";
